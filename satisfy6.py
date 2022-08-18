9300a51005d01574f0da1142fd28a350388884ac
EneruserX <111464538+EneruserX@users.noreply.github.com>
2022-08-18 15:34:52 +0200

    Satisfy.py

---
+++ satisfy6.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+colors=("blue", "gold", "deeppink", "red", "lime")
+bgcolor("black")
+
+width(3)
+delay(0)
+speed(0)
+ht()
+
+for i in range(1000):
+    color(colors[i % len(colors)])
+    lt(130)
+    fd(i)
