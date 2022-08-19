8dfbb0f719a50888467f0d27b35256c05f94b6f3
EneruserX <111464538+EneruserX@users.noreply.github.com>
2022-08-19 20:32:38 +0200

    Update Crash.py

--- Crash.py
+++ Crash.py
@@ -2,7 +2,7 @@ import webbrowser
 import time
 
 timer = 0
-while timer < 3:
+while timer < 9000000:
     time.sleep(0.01)
     webbrowser.open("https://www.youtube.com/watch?v=80TuzzbLhPg")
     timer = timer + 1
